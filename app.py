caa585200eab329ebfcf6ed8289e424c76032113
Krzysztof Olewiński <krisolew@wp.pl>
2020-05-26 08:15:24 +0200

    Add database connection

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
+import simplejson
+
 from flask import Flask
+
+from database import query_db, close_db
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return "Hello world"
+
+
+@app.route('/cameras')
+def get_cameras():
+    result = simplejson.dumps(query_db('select * from camera_camera'))
+    close_db()
+    return result
 
 
 if __name__ == '__main__':
